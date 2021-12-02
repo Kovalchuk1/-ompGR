46a7feec360eed51694e704943546568f9bbf19d
Kovalchuk1 <72132862+Kovalchuk1@users.noreply.github.com>
2021-12-02 02:20:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-import pandas as pd
+from PIL import Image, ImageColor
+from PIL import ImageDraw
+from math import *
 import numpy as np
-from scipy.spatial import ConvexHull
-import matplotlib.pyplot as plt
 
-name = "DS9.txt"
-Data = pd.read_csv(name, sep=" ", header=None, names=["x", "y"])
-Data2 = pd.DataFrame()
-
-points = np.array(Data)
-hull = ConvexHull(points)
-
-ds = plt.figure()
-axes = ds.add_axes((0, 0, 1, 1))
-plt.axis('off')
-
-axes.scatter(Data["x"], Data["y"], color='black')
-plt.xlim([0, 540])
-plt.ylim([960, 0])
-axes.plot(points[hull.vertices, 0, ], points[hull.vertices, 1], 'b-', lw=4)
-plt.savefig('DS9.jpg')
-
-axes2 = ds.add_axes((0, 0, 1, 1))
-axes2.plot(points[hull.vertices, 0], points[hull.vertices, 1], 'b-', lw=4)
-plt.xlim([0, 540])
-plt.ylim([960, 0])
-plt.savefig('convexShell.jpg')
-
-
+ANGLE = radians(-10)
+NewImage = Image.new("RGB", (960, 960))
+PaintingImage = ImageDraw.Draw(NewImage)
+Data = np.loadtxt("DS9.txt", delimiter=' ', skiprows=0, dtype=str)
+for i in Data:
+    p = lambda x: int(x.item()) - 480
+    a = [p(i[0]), p(i[1])]
+    AffineA = [a[0] * cos(ANGLE) - a[1] * sin(ANGLE) + 480,
+               a[0] * sin(ANGLE) + a[1] * cos(ANGLE) + 480]
+    PaintingImage.point((int(AffineA[0]), int(AffineA[1])), "white")
+NewImage.save("AthenianTransformation.png", "PNG")
